f0d299dec39ece129426f528a6ef6a6347646d90
lizheming <i@imnerd.org>
2020-03-28 00:59:11 +0800

    update hooks

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import IPut from 'iput';
 
-class Main extends React.Component {
-  state = {
-    ip: ''
-  }
+function Main() {
+  const [ip, setIP] = useState('');
 
-  render() {
-    return (
-      <div id="container">
-        <h2>Default</h2>
-        <IPut />
-        <h2>With default value</h2>
-        <IPut defaultValue="192.168.1.1" />
-        <h2>Set value when onChange</h2>
-        <IPut onChange={val => this.setState({ip: val})} />
-        <div>IP you input is: {this.state.ip}</div>
-        <h2>return error when ip is 127.0.0.1</h2>
-        <IPut isError={(val)=> val === '127.0.0.1'} />
-      </div>
-    )
-  }
+  return (
+    <div id="container">
+      <h2>Default</h2>
+      <IPut />
+      <h2>With default value</h2>
+      <IPut defaultValue="192.168.1.1" />
+      <h2>Set value when onChange</h2>
+      <IPut onChange={setIP} />
+      <div>IP you input is: {ip}</div>
+      <h2>return error when ip is 127.0.0.1</h2>
+      <IPut isError={(val)=> val === '127.0.0.1'} />
+    </div>
+  )
 }
 
 ReactDOM.render(
